501216162999205ce42eba4a66d021bb5d84e3cf
Zorni97 <raulzornilebron@gmail.com>
2020-05-14 09:56:35 +0200

    Requirements añadidos

--- __main__.py
+++ __main__.py
@@ -5,10 +5,10 @@ parser = ArgumentParser(description="NLP Twitter Analisis")
 parser.add_argument("-u", "--usuario", type=str, help="Nombre de usuario Twitter")
 args = parser.parse_args()
 
-CONSUMER_KEY = "XXXXXXXXXXXXXXXXXXX"
-CONSUMER_SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXX"
-ACCESS_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
-ACCESS_TOKEN_SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
+CONSUMER_KEY = "KMaDnvMUxzVKBwkgXIvB72Slp"
+CONSUMER_SECRET = "FoeYigTb06lQhoXqeIhiIGKanfw4t69ay101ZxMHsMUdrpvlV9"
+ACCESS_TOKEN = "1072524873220702208-9O6qIlnUZAG9JSJrBgqSLPQnv1bmwI"
+ACCESS_TOKEN_SECRET = "OOCHaSE9aV47TMGiVBEz5vGuMs3WP6cqlzdZzVu804YHq"
 
 if args.usuario is None:
     raise Exception("Argumento -u no encontrado")
